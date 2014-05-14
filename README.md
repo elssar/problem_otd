8d20e2971a5a7fe464c8b2be263732e734ed2592
elssar <elssar@altrawcode.com>
2014-05-15 04:51:29 +0530

    Fixed README.md and added comments to code

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ problem_otd
 ===========
 
 Solutions for Problem of the Day - http://www.problemotd.com/
+
+Solving problems from Problem of the Day to learn Go. Accompanied by Python solutions as well.
--- go/src/character_frequency/main.go
+++ go/src/character_frequency/main.go
@@ -4,25 +4,65 @@ import (
     "bufio"
     "fmt"
     "os"
+    "sort"
     "strings"
 )
+
+// Solution for Problem of the Day for 2014-05-12, Character Frequency
+// http://www.problemotd.com/problem/character-frequency/
+
+type CharFreq struct {
+    Char rune
+    Count int
+}
+
+type ByCount []CharFreq
+
+func (a ByCount) Len() int {
+    return len(a)
+}
+
+func (a ByCount) Swap(i, j int) {
+    a[i], a[j] = a[j], a[i]
+}
+
+// Reversed, so that sort is in descending order
+func (a ByCount) Less(i, j int) bool {
+    return a[i].Count > a[j].Count
+}
+
+func InCharFreq(this []CharFreq, x rune) int {
+    i, j := 0, len(this)
+    for i < j {
+        if this[i].Char == x {
+            return i
+        }
+        i += 1
+    }
+    return -1
+}
 
 func main() {
     reader := bufio.NewReader(os.Stdin)
     fmt.Println("Enter text: ")
     text, _ := reader.ReadString('\n')
-    frequency := make(map[uint8]int)
-    for i := 0; i < len(text); i++ {
-        if frequency[text[i]] != 0 {
-            frequency[text[i]] += 1
+    frequency := []CharFreq{}
+
+    for _, x := range text {
+        index := InCharFreq(frequency, x)
+        if index < 0 {
+            frequency = append(frequency, CharFreq{x, 1})
         } else {
-            frequency[text[i]] = 1
+            frequency[index].Count += 1
         }
     }
-    for k, v := range(frequency) {
-        if k != '\n' {
-            count_in_dots := strings.Repeat(".", v)
-            fmt.Printf("%q:\t%q\n", k, count_in_dots)
+
+    sort.Sort(ByCount(frequency))
+
+    for _, v := range(frequency) {
+        if v.Char != '\n' {
+            count_in_dots := strings.Repeat(".", v.Count)
+            fmt.Printf("%q:\t%q\n", v.Char, count_in_dots)
         }
     }
 }
